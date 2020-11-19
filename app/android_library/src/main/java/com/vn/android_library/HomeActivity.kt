eae0b8a03dacde0897712dbb61a7853832a080b6
Tan <lytan56@gmail.com>
2020-11-20 00:51:37 +0700

    Project init

--- app/android_library/src/main/java/com/vn/android_library/HomeActivity.kt
+++ app/android_library/src/main/java/com/vn/android_library/HomeActivity.kt
@@ -12,9 +12,9 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+//        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+//        }
     }
 }
